ddec3e3f2e4743454dfe1177d067bd1846a51a73
Anna S <annaprograms365@gmail.com>
2023-02-14 13:04:14 -0500

    Added exercise that decides whether a value is in a hash

---
+++ Ruby_Book/Chapter7/find_hash_value?.rb
@@ -0,0 +1,7 @@
+birthday_month = {Helen: "January", Matt: "October", Terri: "July"}
+
+if birthday_month.value?("January")
+  puts "Someone has a birthday in that month!"
+else
+  puts "No one has a birthday in that month!"
+end
