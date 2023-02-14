508bb16a5c12b884f52f43eced5875e5d9efa88d
Anna S <annaprograms365@gmail.com>
2023-02-14 11:19:21 -0500

    Added exercise that prints an array of sibling names

---
+++ Ruby_Book/Chapter7/sibling_array.rb
@@ -0,0 +1,13 @@
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank","rob","david"],
+            aunts: ["mary","sally","susan"]
+}
+
+siblings = family.select do |k, v |
+  ( k == :sisters) || ( k == :brothers)
+end
+
+array = siblings.values.flatten
+
+p array
