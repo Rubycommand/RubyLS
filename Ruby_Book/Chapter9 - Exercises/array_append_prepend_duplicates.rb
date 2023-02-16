f8917d91baf662410f443d11d390998d50649d37
Anna S <annaprograms365@gmail.com>
2023-02-16 11:17:34 -0500

    Added exercise that modifies an array in multiple ways

---
+++ Ruby_Book/Chapter9 - Exercises/array_append_prepend_duplicates.rb
@@ -0,0 +1,14 @@
+#Exercises 4, 5, 6
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+array.push(11)
+array.unshift(0)       #Exercise 4
+p array
+
+array.pop
+array.push(3)  #Exercise 5
+p array
+
+array.uniq!           #Exercise 6
+p array
