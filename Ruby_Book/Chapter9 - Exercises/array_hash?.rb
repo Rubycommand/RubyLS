e36b5bd5fabbb081b773de7822407859c23afd99
Anna S <annaprograms365@gmail.com>
2023-02-16 15:18:11 -0500

    Added exercise that shows an array of hashes, and a hash of arrays.

---
+++ Ruby_Book/Chapter9 - Exercises/array_hash?.rb
@@ -0,0 +1,8 @@
+favorite_colors_hash = {harold: ["yellow", "green"], matt: ["purple", "red"], helen: ["taupe", "mauve"]}
+p favorite_colors_hash[:matt] #all of these hash values are arrays
+
+array_of_hashes = [{mouse: "mammal", eagle: "bird"}, {frog: "amphibian", iguana: "reptile"}]
+p array_of_hashes[0] #this is an array of hashes
+
+
+
