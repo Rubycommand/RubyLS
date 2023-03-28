38c2e78a608fd9f24bf661d15c6fb81909fd2e8f
Anna S <annaprograms365@gmail.com>
2023-03-28 11:50:22 -0400

    Added an exercise that explores variable assignment and change.

---
+++ Launch_School/RB101/small_exercises/easy2/string_assignment.rb
@@ -0,0 +1,10 @@
+name = 'Bob'
+save_name = name
+name = 'Alice'
+puts name, save_name # Alice, Bob. name was changed to point to 'Alice', but save_name still points to 'Bob'.
+
+
+name = 'Bob'
+save_name = name # Here, name and save_name both point to 'Bob'
+name.upcase!     # now, name is destructively changed. Old 'Bob' no longer exists.
+puts name, save_name # Here, both name and save_name point to 'BOB'
