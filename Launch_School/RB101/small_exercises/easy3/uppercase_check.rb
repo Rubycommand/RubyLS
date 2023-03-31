8d70a4b8a6eb91ed89f989e763a51538926460b5
Anna S <annaprograms365@gmail.com>
2023-03-31 11:33:22 -0400

    Added a method that determines whether all letters in a string are capitalized.

--- Launch_School/RB101/small_exercises/easy3/uppercase_check.rb
+++ Launch_School/RB101/small_exercises/easy3/uppercase_check.rb
@@ -2,9 +2,9 @@ def uppercase?(string)
   string == string.upcase
 end
 
-puts uppercase?('t') == false
-puts uppercase?('T') == true
-puts uppercase?('Four Score') == false
-puts uppercase?('FOUR SCORE') == true
-puts uppercase?('4SCORE!') == true
-puts uppercase?('') == true
+uppercase?('t')
+uppercase?('T')
+uppercase?('Four Score')
+uppercase?('FOUR SCORE')
+uppercase?('4SCORE!')
+uppercase?('')
